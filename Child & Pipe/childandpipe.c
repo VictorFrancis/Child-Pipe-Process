94cbf5112c2fe4c26c3e2d2c409a675cd46ae03c
Victor Francis <47282417+VictorFrancis@users.noreply.github.com>
2020-09-04 05:55:48 +0600

    Add files via upload

---
+++ Child & Pipe/childandpipe.c
@@ -0,0 +1,45 @@
+#include <sys/ipc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(){
+    int processPipe[6];
+    char writemessage[3][100];
+    char readmessage[100];
+    //initializing pipe
+    for(int i=0;i<3;i++){
+        pipe(&processPipe[2*i]);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if(fork()==0){
+            printf("writing child process %d\n",i);
+            int j=0;
+            int counter=0;
+            while(1){
+                char in;
+                scanf("%c",&in);
+                if(in == '\n'){
+                    counter++;
+                    if(counter==2){
+                        break;
+                    }
+                }else{
+                    writemessage[i][j]=in;
+                    j++;
+                }
+            }
+            write(processPipe[2*i+1],writemessage[i],sizeof(writemessage[i]));
+            exit(0);
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        wait(NULL);
+        read(processPipe[2*i],readmessage,sizeof(readmessage));
+        printf("\nMessage from child process %d Message: %s",i,readmessage);
+    }
+}
+//final commit
